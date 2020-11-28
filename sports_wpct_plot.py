cfb5ad44c459005249d21b2ad3f59c6d0c4961d6
Joseph Engelhardt <44878057+jgengelhardt@users.noreply.github.com>
2020-11-27 22:22:14 -0600

    Update sports_wpct_plot.py

--- sports_wpct_plot.py
+++ sports_wpct_plot.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 from matplotlib import pylab
 
 # define datasets and labels
-leagues = (NHL_history, NBA_history)
-colors = ('k', '#FA8320')
-groups = ('NHL','NBA')
+leagues = (NHL_history, NBA_history, WNBA_history, NFL_history)
+colors = ('#000000', '#C9082A','#FA8320', '#013369')
+groups = ('NHL','NBA', 'WNBA', 'NFL')
 
 # plot data grouped by league
 for league, color, group in zip(leagues, colors, groups):
     x,y = zip(*league)
-    plt.scatter(x, y, c=color, label=group)
+    plt.scatter(x, y, c=color, alpha=0.5, label=group)
     z = numpy.polyfit(x, y, 1)
     p = numpy.poly1d(z)
     pylab.plot(x,p(x),color)
 
-# design layout
+# design
 plt.title('How Well Does Win% Represent Skill?')
 plt.ylabel('Influence of Skill')
 plt.xlabel('Season')
