3727e783dff297435d22359f0d7b9a7ff145ffe8
Joseph Engelhardt <44878057+jgengelhardt@users.noreply.github.com>
2020-11-27 23:39:36 -0600

    Update sports_wpct_plot.py

--- sports_wpct_plot.py
+++ sports_wpct_plot.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from matplotlib import pylab
 
 # define datasets and labels
-leagues = (NHL_history, NBA_history, WNBA_history, NFL_history)
-colors = ('#000000', '#C9082A','#FA8320', '#013369')
-groups = ('NHL','NBA', 'WNBA', 'NFL')
+leagues = (NHL_history, NBA_history, WNBA_history, NFL_history, MLB_history)
+colors = ('#000000', '#C9082A','#FA8320', '#013369','y')
+groups = ('NHL','NBA', 'WNBA', 'NFL','MLB')
 
 # plot data grouped by league
 for league, color, group in zip(leagues, colors, groups):
     x,y = zip(*league)
-    plt.scatter(x, y, c=color, alpha=0.5, label=group)
+    plt.scatter(x, y, c=color, alpha=0.2, label=group)
     z = numpy.polyfit(x, y, 1)
     p = numpy.poly1d(z)
     pylab.plot(x,p(x),color)
