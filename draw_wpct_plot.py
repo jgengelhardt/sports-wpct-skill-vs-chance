a0389802731f017723604c555d6bdc5d326367f5
Joseph Engelhardt <44878057+jgengelhardt@users.noreply.github.com>
2020-12-05 10:47:16 -0600

    Add NCAABB stats

--- draw_wpct_plot.py
+++ draw_wpct_plot.py
@@ -1,21 +1,22 @@
 import numpy
 import matplotlib.pyplot as plt
 from matplotlib import pylab
+plt.figure(figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
 
 # define datasets and labels
-leagues = ('NHL','NBA', 'WNBA', 'NFL','MLB')
-colors = ('#8394a1', '#C9082A','#FA8320', '#013369','#37ae0f')
+leagues = ('NHL','NBA', 'WNBA', 'NCAABB', 'NFL','MLB')
+colors = ('#8394a1', '#C9082A','#FA8320', '#005eb8', '#013369','#37ae0f')
 
 # plot data grouped by league
 for league, color in zip(leagues, colors):
     x=[]
     y=[]
-    with open(f'./scrapers/results/{league}_history.csv', 'r') as csvfile:
+    with open(f'./results/{league}_history.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
             x.append(int(row[0]))
             y.append(float(row[1]))
-    plt.scatter(x, y, c=color, alpha=0.3, label=league)
+    plt.scatter(x, y, c=color, alpha=0.2, label=league)
     z = numpy.polyfit(x, y, 1)
     p = numpy.poly1d(z)
     pylab.plot(x,p(x),color)
---
+++ scrapers/NCAABB_scraper.py
@@ -0,0 +1,52 @@
+def get_season_var_NCAABB(year):
+    try:     # check if the URL exists; else print('No season played')
+        year_html = urlopen(f'https://www.sports-reference.com/cbb/seasons/{year}.html')
+    except HTTPError as e:
+        return 'Error'
+    year_bs = BeautifulSoup(year_html, 'html.parser')
+    year_comments = year_bs.find_all(text=lambda text:isinstance(text, Comment))
+    season_tables = year_bs.findAll('table')
+    if len(season_tables) == 0:
+        return 'Error'
+    # pull all win percentages into a list
+    win_pct_raw = []
+    for table in season_tables:
+        win_pct_raw += (table.findAll('td',attrs={'data-stat':'win_loss_pct'}))
+    win_pct = []
+    for score in win_pct_raw:
+        try:
+            win_pct.append(float(score.get_text()))
+        except:
+            return 'Error'
+    # find variance attributable to chance
+    flips = []
+    for table in season_tables:
+        for row in table.findAll('tr'):
+            try:
+                flips.append(int(table.find('td',{'data-stat':'wins'}).get_text())+int(table.find('td',{'data-stat':'losses'}).get_text()))
+            except:
+                pass
+    if len(flips) <= 0:
+        return 'Error'
+    flips = sum(flips)/len(flips) # mean number of games played (not ideal, but these tables sometimes include playoff games)
+    # get observed variance minus variance attributable to chance
+    var_diff = variance(win_pct)-(0.5*0.5/flips)
+    if var_diff > 0:
+        return round(sqrt(var_diff),3) #return standard deviation, not variance, for readability
+    else:
+        return 'Error'
+
+def get_NCAABB_history():
+    csvFile = open('./results/NCAABB_history.csv', 'w', newline='')
+    try:
+        writer = csv.writer(csvFile)
+        for i in range(1971,2020):
+            score = [i, get_season_var_NCAABB(i)]
+            if 'Error' not in score:
+                print (f'Loaded NCAABB season {i}', end="\r", flush=True)
+                writer.writerow(score)
+    finally:
+        csvFile.close()
+        print('\nNCAABB .csv written', flush=True)
+
+get_NCAABB_history()
