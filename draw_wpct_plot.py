d6b162acc997114466831f43ee7158c2136e9afd
Joseph Engelhardt <44878057+jgengelhardt@users.noreply.github.com>
2020-12-13 16:55:56 -0600

    Use a polynomial regression

--- draw_wpct_plot.py
+++ draw_wpct_plot.py
@@ -1,27 +1,26 @@
-import numpy
 import matplotlib.pyplot as plt
 from matplotlib import pylab
-plt.figure(figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+import numpy
+import pandas as pd
+
 
 # define datasets and labels
-leagues = ('NHL','NBA', 'WNBA', 'NCAABB', 'NFL','MLB')
-colors = ('#8394a1', '#C9082A','#FA8320', '#005eb8', '#013369','#37ae0f')
+leagues = ('NHL','NBA', 'NCAABB', 'NFL','MLB')
+colors = ('#8394a1', '#C9082A', '#005eb8', '#013369','#37ae0f')
 
 # plot data grouped by league
+col_names = ['year','variance','games']
 for league, color in zip(leagues, colors):
-    x=[]
-    y=[]
-    with open(f'./results/{league}_history.csv', 'r') as csvfile:
-        plots = csv.reader(csvfile, delimiter=',')
-        for row in plots:
-            x.append(int(row[0]))
-            y.append(float(row[1]))
+    df = pd.read_csv(f'./results/{league}_history.csv', names=col_names)
+    x = df['year']
+    y = df['variance']
     plt.scatter(x, y, c=color, alpha=0.2, label=league)
-    z = numpy.polyfit(x, y, 1)
+    z = numpy.polyfit(x, y, 2)
     p = numpy.poly1d(z)
     pylab.plot(x,p(x),color)
 
 # design
+# plt.figure(figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
 plt.title('How Well Does Win% Represent Skill?')
 plt.ylabel('Influence of Skill')
 plt.xlabel('Season')
