4540c3492a52c82b9f85ac8c7662f477b0abb56a
Joseph Engelhardt <44878057+jgengelhardt@users.noreply.github.com>
2020-12-14 10:07:03 -0600

    Smooth trend line

--- draw_games_plot.py
+++ draw_games_plot.py
@@ -1,6 +1,4 @@
 import matplotlib.pyplot as plt
-from matplotlib import pylab
-import numpy
 import numpy.polynomial.polynomial as poly
 import pandas as pd
 
@@ -8,7 +6,6 @@ plt.figure(figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
 
 # define datasets and labels
 leagues = ('NHL','NBA', 'NCAABB', 'NFL','MLB')
-colors = ('#8394a1', '#C9082A', '#005eb8', '#013369','#37ae0f')
 
 # aggregate league data
 def plot_by_games():
@@ -19,11 +16,11 @@ def plot_by_games():
         df = pd.read_csv(f'./results/{league}_history.csv', names=col_names)
         x += df['games'].tolist()
         y += df['variance'].tolist()
-
+    
     plt.scatter(x, y, c='k', alpha=0.2)
     coefs = poly.polyfit(x, y, 3)
-    ffit = poly.polyval(x, coefs)
-    plt.plot(x, ffit)
+    ffit = poly.polyval(sorted(x), coefs)
+    plt.plot(sorted(x), ffit)
     
     # design
     plt.title('How Well Does Win% Represent Skill?')
