8813b5c317208728eb870be75dc2754d11a3ff9f
Joseph Engelhardt <44878057+jgengelhardt@users.noreply.github.com>
2020-12-13 22:02:18 -0600

    Update poly funct and localize variables
    The numpy documentation specifies to use a different function for polynomials for stability.

--- draw_wpct_plot.py
+++ draw_wpct_plot.py
@@ -1,29 +1,31 @@
 import matplotlib.pyplot as plt
 from matplotlib import pylab
 import numpy
+import numpy.polynomial.polynomial as poly
 import pandas as pd
 
-fig= plt.figure(figsize=(8,6))
+plt.figure(figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
 
 # define datasets and labels
 leagues = ('NHL','NBA', 'NCAABB', 'NFL','MLB')
 colors = ('#8394a1', '#C9082A', '#005eb8', '#013369','#37ae0f')
 
-# plot data grouped by league
-col_names = ['year','variance','games']
-for league, color in zip(leagues, colors):
-    df = pd.read_csv(f'./results/{league}_history.csv', names=col_names)
-    x = df['year']
-    y = df['variance']
-    plt.scatter(x, y, c=color, alpha=0.2, label=league)
-    z = numpy.polyfit(x, y, 2)
-    p = numpy.poly1d(z)
-    pylab.plot(x,p(x),color)
+def plot_by_league():
+    col_names = ['year','variance','games']
+    for league, color in zip(leagues, colors):
+        df = pd.read_csv(f'./results/{league}_history.csv', names=col_names)
+        x = df['year']
+        y = df['variance']
+        plt.scatter(x, y, c=color, alpha=0.2, label=league)
+        coefs = poly.polyfit(x, y, 2)
+        ffit = poly.polyval(x, coefs)
+        pylab.plot(x,ffit,color)
 
-# design
-# plt.figure(figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
-plt.title('How Well Does Win% Represent Skill?')
-plt.ylabel('Influence of Skill')
-plt.xlabel('Season')
-plt.legend(loc=0)
-plt.show()
+    # design
+    plt.title('How Well Does Win% Represent Skill?')
+    plt.ylabel('Influence of Skill')
+    plt.xlabel('Season')
+    plt.legend(loc=0)
+    plt.show()
+    
+plot_by_league()
